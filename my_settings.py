c4a9dfe8a2be1fe350179983dc291bf21638b012
이수진 <s2jiin@isujin-ui-MacBookPro.local>
2021-03-15 18:07:22 +0900

    update : gitignore

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'fd7j9oz@qaxj0$6hbe!6ul1ogakz%g^k$)z$(@@f+bopzj(%2d'
+
+DATABASES = {
+'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kkakka_db',
+        'USER': 'root',
+        'PASSWORD': '9316Jibang!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
