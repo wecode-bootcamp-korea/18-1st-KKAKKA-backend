06828ac94f98cc97041a4a5eadc904f60b3004b0
이수진 <s2jiin@isujin-ui-MacBookPro.local>
2021-03-24 14:12:12 +0900

    REST API 적용 완료

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views      import ProductView, ProductDetailView
 
 urlpatterns = [
-    path('/products', ProductView.as_view()),
-    path('/contents/<int:product_id>', ProductDetailView.as_view())
+    path('/', ProductView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view())
 ]
