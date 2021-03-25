06662beb7ad8338d5c371cc84c84e3a372da9132
이수진 <s2jiin@isujin-ui-MacBookPro.local>
2021-03-25 19:23:59 +0900

    리뷰 반영 완료

--- order/views.py
+++ order/views.py
@@ -10,25 +10,25 @@ from subscription.models  import Subscription, SubscriptionPlan
 from account.models       import Account
 from product.models       import OptionSubscription
 
-#subscription 주문
+
 class SubscriptionOrderView(View):
     # @Validator
-    # @transaction 쓰기
+    # @transaction
     def post(self, request, subscription_id):
         try:
             data = json.loads(request.body)
 
-            account           = data['account']          
-            monthly_plan      = data['monthly_plan']
-            quantity          = data['quantity']
-            delivery_date     = data['delivery_date']
-            option            = data['option']
+            account_id           = data['account']          
+            monthly_plan_id      = data['monthly_plan']
+            quantity             = data['quantity']
+            delivery_date        = data['delivery_date']
+            option_id            = data['option']
 
             temp_date         = parse_date(delivery_date)
             status            = Status.objects.get(id = 1)
-            account           = Account.objects.get(id = account)
-            option            = OptionSubscription.objects.get(id = option)
-            subscriptionplans = SubscriptionPlan.objects.get(subscription_id = subscription_id, monthly_plan_id = monthly_plan)
+            account           = Account.objects.get(id = account_id)
+            option            = OptionSubscription.objects.get(id = option_id)
+            subscriptionplans = SubscriptionPlan.objects.get(subscription_id = subscription_id, monthly_plan_id = monthly_plan_id)
 
             order = Order.objects.create(account_id = account.id, status_id = status.id)
 
