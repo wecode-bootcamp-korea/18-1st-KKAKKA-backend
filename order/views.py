45d9513e5941ea5c7187f7cfc110fda05feac24a
이수진 <s2jiin@isujin-ui-MacBookPro.local>
2021-03-25 16:04:29 +0900

    subscription 수정 전

--- order/views.py
+++ order/views.py
@@ -7,6 +7,7 @@ from django.views           import View
 
 from .models              import Order, Status, SubscriptionCart
 from subscription.models  import Subscription, SubscriptionPlan
+from account.models       import Account
 
 #subscription 주문
 class SubscriptionOrderView(View):
@@ -15,24 +16,28 @@ class SubscriptionOrderView(View):
     def post(self, request, subscription_id):
         try:
             data = json.loads(request.body)
-            
-            account           = data['account']
-            monthly_plan      = data['monthly_plan'] #정기결제->plan # string으로 들어온단밀이지? id값으로 보내주는 지 확인
+
+            account           = data['account']          
+            monthly_plan      = data['monthly_plan']
             quantity          = data['quantity']
-            delivery_date     = data['dlivery_date'] #"2021-03-26"
+            delivery_date     = data['delivery_date']
 
-            #order 생성, status default = '결제 전'
-            order = Order.objects.create(account_id = account, status_id = Order.status.get(id=1))
+            # order 생성, status default = '결제 전'
+            status = Status.objects.get(id = 1)
+            account = Account.objects.get(id = account)
 
-            #subsriptionCart
-            temp_date = parse_date(delivery_date)
+            order = Order.objects.create(account_id = account.id, status_id = status.id)
 
-            SubscriptionCart.objects.create(
-                order         = order.id,
-                subscription  = SubscriptionPlan.filter(subscription = subscription_id, monthly_plan = monthly_plan),
-                quantity      = int(quantity),
-                delivery_date = temp_date
-            )
+            # subsriptionCart
+            if order:
+                temp_date = parse_date(delivery_date)
+
+                SubscriptionCart.objects.create(
+                    order         = order.id,
+                    subscription  = SubscriptionPlan.objects.filter(subscription = subscription_id, monthly_plan = monthly_plan),
+                    quantity      = quantity,
+                    delivery_date = temp_date
+                    )
 
             return JsonResponse({'message':'SUCCESS'}, status=200)
 
